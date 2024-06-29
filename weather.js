736621a8070851609246cace3ba5fea6fcb17ea4
kovaaalex <kovalenko.alex04@gmail.com>
2024-06-30 01:40:18 +0300

    select time hourly

--- weather.js
+++ weather.js
@@ -1,15 +1,28 @@
-const apiKey = "apiKey"
-const city = "Петриков"
-fetch(`https://api.weatherbit.io/v2.0/forecast/hourly?city=Petrikov&key=8527ffdd784d4f81ac1f1a53d8f718cb&hours=48`)
-.then(response => response.json())
-.then(data => {
-  let temperatures = ""
-  data.data.forEach(day => {
-    document.querySelector('.weather__container h3').innerHTML = `${day.city_name}, ${day.country_code}`
-    temperatures += `Date: ${day.datetime}, Temp: ${day.temp}\n${data.data[0].city_name}`;
+const apiKey = "8527ffdd784d4f81ac1f1a53d8f718cb"
+let city = "Петриков"
+function fromCurrentTime(currentWeather){
+  const regex = /T(.*)/s
+  const match = currentWeather.timestamp_local.match(regex)[1].slice(0, 5)
+  const weatherContainer = document.querySelector('.weather__hourly__container')
+  weatherContainer.innerHTML += 
+  `<div class='hour__weather'>
+    <h5>${match}</h5>
+    ${currentWeather.weather.icon}
+  </div>`
+}
+function showWeather(){
+  fetch(`https://api.weatherbit.io/v2.0/forecast/hourly?city=${city}&key=8527ffdd784d4f81ac1f1a53d8f718cb&hours=48`)
+  .then(response => response.json())
+  .then(data => {
+    let temperatures = ""
+    data.data.forEach(day => {
+      document.querySelector('.forecast__container h3').innerHTML = `${data.city_name}, ${data.country_code}`
+      temperatures += `Date: ${day.datetime}, Temp: ${day.temp}\n${data.data[0].city_name}`
+      fromCurrentTime(day)
+    })
   })
-  alert(temperatures)
-})
-.catch(alert(error))
+  .catch(alert(error))
+}
+showWeather()
 
 
